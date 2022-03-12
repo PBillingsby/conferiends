012dca225633ecc74d2d7cfe380c2a7498607ad5
PBillingsby <philbillingsby@gmail.com>
2022-03-11 21:52:19 -0800

    Remove button and modal component

--- components/BeginModal.tsx
+++ components/BeginModal.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import {
   Modal,
   ModalOverlay,
@@ -6,36 +8,27 @@ import {
   ModalCloseButton,
   Button,
   Box,
-  Link,
   Flex,
-  Spacer,
-  Heading,
+  Text,
   useDisclosure
 } from "@chakra-ui/react"
 
 export default function BeginModal() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Box>
-      <Button size='lg' bg='none' border='1.5px solid black' fontSize='xl' boxShadow='0px 0px 15px #a5a5a5' _hover={{ boxShadow: '0px 0px 35px #a5a5a5', bg: '#eee', color: 'black' }} color='#fff' onClick={() => onOpen()}>
-        Get Started
-      </Button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay bg='none'
-          backdropFilter='auto'
-          backdropInvert='10%'
-          backdropBlur='3px'
-        />
-        <ModalContent pt={5}>
-          <ModalCloseButton />
-          <ModalBody>
-            <Heading size='md'>Choose your destiny</Heading>
-            <Flex pt={4}>
-
-            </Flex>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <ModalOverlay bg='none'
+        backdropFilter='auto'
+        backdropInvert='10%'
+        backdropBlur='3px'
+      />
+      <ModalContent pt={6}>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>Hey</Text>
+          <Flex pt={4}>
+          </Flex>
+        </ModalBody>
+      </ModalContent>
     </Box>
   )
 }
