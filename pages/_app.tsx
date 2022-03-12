27feef237d51779f71ee8db9db3c2b5f081d7f17
PBillingsby <philbillingsby@gmail.com>
2022-03-11 21:54:41 -0800

    Removes src dir

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,19 @@ import { AppProps } from 'next/app'
 import { ChakraProvider, Box, Container, extendTheme } from '@chakra-ui/react'
 import { Provider, NETWORKS } from '@web3-ui/core';
 import Navbar from '../components/NavBar'
-import bg from '../public/bg.png'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Provider network={NETWORKS.mainnet}>
         <Box pt='4' h='100vh'
+          bg='none'
           backgroundRepeat='no-repeat'
           backgroundSize='fit-content'
-          backgroundImage={bg}
+          backgroundImage="url('../public/bg.png')"
         >
           <Navbar />
-          <Container pt={30} maxW='container.lg'>
+          <Container maxW='container.lg'>
             <Component {...pageProps} />
           </Container>
         </Box>
