09774974c1e333f5aaa3edee3b47cc5a4c02e2f6
PBillingsby <philbillingsby@gmail.com>
2022-03-11 21:54:00 -0800

    Remove wallet connect from navbar. fix underline syntax errors

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,24 +1,8 @@
 import NextLink from "next/link"
-import { useWallet } from '@web3-ui/core';
-
-import { Button, Spacer, Icon, HStack, Heading, Link } from '@chakra-ui/react';
+import { Spacer, Icon, HStack, Heading, Link } from '@chakra-ui/react';
 import { Ticket } from 'tabler-icons-react';
 
 export default function NavBar() {
-  const {
-    disconnectWallet,
-    connected,
-    connection,
-    connectWallet,
-    switchToCorrectNetwork
-  } = useWallet();
-
-  const ethAddress = () => {
-    return `${connection.userAddress!.slice(0, 4)
-      }...${connection.userAddress!.slice(connection.userAddress!.length - 4)
-      } `
-  }
-
   return (
     <HStack px={8} pb={8} w='100vw' position='fixed'>
       <Icon as={Ticket} w={8} h={8} />
@@ -33,7 +17,7 @@ export default function NavBar() {
         <NextLink href={'/pitches'} passHref>
           <Link>Fund Others</Link>
         </NextLink>
-      </HStack>
-    </HStack>
+      </HStack >
+    </HStack >
   )
 }
