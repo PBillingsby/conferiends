21de9318c1ec818b7baa7f457a0fd6e556597fe6
PBillingsby <philbillingsby@gmail.com>
2022-03-13 09:34:31 -0700

    Override and set default darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import { AppProps } from 'next/app'
-import { ChakraProvider, Box, Container } from '@chakra-ui/react'
+import { ChakraProvider, Box, Container, DarkMode } from '@chakra-ui/react'
 import { Provider, NETWORKS } from '@web3-ui/core';
 import Navbar from '../components/NavBar'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Provider network={NETWORKS.mainnet}>
-        <Box pt='4' h='100vh'>
-          <Navbar />
-          <Container maxW='container.lg' pt={12}>
-            <Component {...pageProps} />
-          </Container>
-        </Box>
+        <DarkMode>
+          <Box pt='4' h='100vh'>
+            <Navbar />
+            <Container maxW='container.lg' pt={12}>
+              <Component {...pageProps} />
+            </Container>
+          </Box>
+        </DarkMode>
       </Provider>
     </ChakraProvider >
   )
