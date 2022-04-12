b01e816be7b63d9354b0274bd996ba96a29d4cf9
PBillingsby <philbillingsby@gmail.com>
2022-04-12 07:34:15 -0700

    add connectWallet function to utils. remove unneeded imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ const theme = extendTheme({
         },
       },
     },
-  }
+  },
 })
 
 
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,27 @@
+import { useToast } from "@chakra-ui/react";
+import { ethers } from 'ethers'
+
 export const addressFormatter = (address) => {
   return `${address.slice(0, 6)}...${address.slice(address.length - 6)}`
+}
+
+export const walletConnect = async (toast) => {
+  if (typeof window.ethereum !== 'undefined') {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+    await provider.send("eth_requestAccounts", []);
+
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+
+    await address
+    return address
+  }
+  else {
+    toast({
+      title: 'Must have MetaMask browser extension',
+      status: 'error',
+      isClosable: true,
+    });
+  }
 }
