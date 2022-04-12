300c5aa7f23fb77214d50c15f8b1625201046567
PBillingsby <philbillingsby@gmail.com>
2022-04-12 07:32:02 -0700

    add mm connect & spinner

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 import { ethers } from 'ethers'
 
 import {
@@ -8,32 +8,47 @@ import {
   Spacer,
   Heading,
   Text,
-  Input,
+  Spinner,
   Box,
   Select,
   Button,
   Textarea,
-  InputGroup,
+  Center,
   NumberInput,
   NumberInputField,
   NumberIncrementStepper,
   NumberDecrementStepper,
   NumberInputStepper,
+  useToast,
 } from "@chakra-ui/react"
 
 import IconVector from '../components/IconVector'
-import Clipboard from './Clipboard';
-import { addressFormatter } from '../utils/index.js'
+import { walletConnect } from '../utils/index.js'
 
-interface WalletAddressProps {
-  walletAddress: string;
+interface FormProps {
   onClose: () => void;
 }
 
-export default function Form(props: WalletAddressProps) {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+export default function Form(props: FormProps) {
+  const [walletAddress, setWalletAddress] = useState('');
+
+  const toast = useToast({
+    containerStyle: {
+      width: '20vw',
+      maxWidth: '100%',
+    },
+  })
+
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(event);
+
+    await isLoading
+    setIsLoading(true);
+    const address = await walletConnect(toast);
+
+    setWalletAddress(address);
+    setIsLoading(false);
   }
 
   return (
@@ -45,33 +60,39 @@ export default function Form(props: WalletAddressProps) {
         <Heading size='lg'>Pitch Form</Heading>
       </Flex>
       <Text as='sup'>tell us more about the event and why you&apos;d like funding to attend</Text>
-      <FormControl>
-        <FormLabel htmlFor='address'>Address</FormLabel>
-        <InputGroup>
-          <Input id='address' textAlign='center' type='text' value={addressFormatter(props.walletAddress)} isReadOnly />
-          <Clipboard walletAddress={props.walletAddress} />
-        </InputGroup>
-        <FormLabel htmlFor='event' pt={2}>Event</FormLabel>
-        <Select placeholder='Select Event' mb={2}>
-          <option value='react-miami'>React Miami</option>
-          <option value='dev-connect'>DevConnect</option>
-        </Select>
-        <FormLabel htmlFor='amount'>Funds Needed</FormLabel>
-        <NumberInput step={0.01} min={0}>
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-        <FormLabel htmlFor='pitch' pt={2}>Pitch</FormLabel>
-        <Textarea id='pitch' h={32} />
-        <Flex mt={6}>
-          <Button size='md' onClick={props.onClose}>Close</Button>
-          <Spacer />
-          <Button size='md' type='submit'>Pitch</Button>
-        </Flex>
+      <FormControl h='55vh'>
+        {isLoading ?
+          <Center pt='20vh'>
+            <Spinner
+              size='xl'
+              color='#C76173'
+            />
+          </Center>
+          :
+          <>
+            <FormLabel htmlFor='event' pt={2}>Event</FormLabel>
+            <Select placeholder='Select Event' mb={2}>
+              <option value='react-miami'>React Miami</option>
+              <option value='dev-connect'>DevConnect</option>
+            </Select>
+            <FormLabel htmlFor='amount'>Funds Needed</FormLabel>
+            <NumberInput step={0.01} min={0}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <FormLabel htmlFor='pitch' pt={2}>Pitch</FormLabel>
+            <Textarea id='pitch' h={32} />
+            <Flex mt={6}>
+              <Button size='md' onClick={props.onClose}>Close</Button>
+              <Spacer />
+              <Button size='md' type='submit'>Pitch</Button>
+            </Flex>
+          </>
+        }
       </FormControl>
-    </form>
+    </form >
   )
 }
